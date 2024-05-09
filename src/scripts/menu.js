1dbef7e047eda0700a2516a18c50ce5aca387f2b
Li0329 <haxk1024@gmail.com>
2024-05-09 20:32:49 +0800

    adjust the distance of browser top

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -16,3 +16,23 @@ document.addEventListener("astro:page-load", function() {
         }
     });
 });
+
+
+function handlePageLoad() {
+    window.toggleMenu = function () {
+      const navLinks = document.getElementById('nav-links');
+      const hamburger = document.querySelector('.hamburger');
+  
+      if (navLinks.classList.contains('open')) {
+        navLinks.classList.remove('open');
+        navLinks.classList.add('collapsed');
+      } else {
+        navLinks.classList.remove('collapsed');
+        navLinks.classList.add('open');
+      }
+  
+      hamburger.classList.toggle('open');
+    }
+  }
+  
+  document.addEventListener('astro:page-load', handlePageLoad);
