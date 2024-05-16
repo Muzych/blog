a0dd898a7c41add20de726a0c358813aedf4b505
Li0329 <haxk1024@gmail.com>
2024-05-17 00:12:20 +0800

    add rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://localhost:4321",
     integrations: [
         UnoCSS({
             injectReset: true
