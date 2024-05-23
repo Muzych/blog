989a0076983d7d4048485416478af7f8909ca5c3
Li0329 <haxk1024@gmail.com>
2024-05-23 16:47:22 +0800

    add web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
+
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ]
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind({
+    applyBaseStyles: false
+  }), react(), partytown()]
 });
