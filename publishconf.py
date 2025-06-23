fbc15d2480fd2f14ad8a2ec9692106413ede745d
Muzych <haxk1024@gmail.com>
2025-06-23 22:50:03 +0800

    upate siteurl

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://muzych.github.io/blog"
+SITEURL = "https://muziyancheng.com"
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = False
 
