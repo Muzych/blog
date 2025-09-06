d1b9af19c59bffb4618a2b21434ac7e39096afac
Muzych <haxk1024@gmail.com>
2025-09-06 23:57:37 +0800

    feat: update to solidstart

---
+++ app.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "@solidjs/start/config";
+/* @ts-ignore */
+import pkg from "@vinxi/plugin-mdx";
+
+const { default: mdx } = pkg;
+export default defineConfig({
+  extensions: ["mdx", "md"],
+  ssr: false, // 禁用服务端渲染
+  experimental: {
+    islands: false
+  },
+  vite: {
+    plugins: [
+      mdx.withImports({})({
+        jsx: true,
+        jsxImportSource: "solid-js",
+        providerImportSource: "solid-mdx"
+      })
+    ]
+  }
+});
--- content/pages/about.md
+++
@@ -1,6 +0,0 @@
-Title: About Me
-Slug: about
-Date: 2025-06-22
-Template: page
-
-Hi, I'm Your Name, and this is my blog built with Pelican!
